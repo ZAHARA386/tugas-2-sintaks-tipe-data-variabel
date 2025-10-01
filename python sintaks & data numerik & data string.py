49dd0b69f51c09f9ce43ca3911c47153e2622f8e
ZAHARA386 <zaharatob001@gmail.com>
2025-10-01 13:08:02 +0700

    Add files via upload

---
+++ python sintaks & data numerik & data string.py
@@ -0,0 +1,25 @@
+ibu_kota = 'pangkal pinang'
+print("ibu_kota")
+
+panjang = 5
+lebar = 10.5 
+luas = panjang * lebar 
+
+print(panjang, '*', lebar, '=' luas)
+print("Tipe dari variabel panjang:", type (panjang))
+print("Tipe dari variabel lebar:", type (lebar))
+print("Tipe dari variabel luas:", type (luas))
+
+nama_depan = "zahara"
+nama_belakang = 'zahara'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '19'
+alamat = 'serdang, toboali, bangka selatan'
+motivasi = "selesaikan apa yang sudah dimulai"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', motivasi:', motivasi)
+
+print('\ntipe dari nama_lengkap:', type(nama_lengkap))
+print('tipe dari usia:', type(usia))
+print('tipe dari alamat:', type(alamat))
+print('tipe dari motivasi:', type(motivasi))
